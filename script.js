1bac10f9c260482376576b117f1e8cc50ba0c90a
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-11 10:06:35 -0700

    formatted card from js html render; commented out card layout and original html renderings

--- script.js
+++ script.js
@@ -8,7 +8,11 @@ const optionsGet = {
 }
 
 let submit = $("#submit");
-let remove = $("#remove-btn5");
+
+function wait(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+wait(2000).then(displayMovies);
 
 function displayMovies() {
     fetch(url, optionsGet)
@@ -17,10 +21,34 @@ function displayMovies() {
             let html = "";
             console.log(movies);
             movies.forEach((movie => {
-                html += `<li>title: ${movie.title}</li><li>genre: ${movie.genre}</li><li>director: ${movie.director}</li><li>actors: ${movie.actors}</li>
-                    <li>id: ${movie.id}</li><li>plot: ${movie.plot}</li><li>poster: ${movie.poster}</li><li>rating: ${movie.rating}</li>
-                    <li>year: ${movie.year}</li><button class="remove-btn" data-id="${movie.id}">DELETE</button><br>`
+                //<div class="card" style="width: 18rem;">
+                //   <img src="..." class="card-img-top" alt="...">
+                //   <div class="card-body">
+                //     <h5 class="card-title">Card title</h5>
+                //     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                //     <a href="#" class="btn btn-primary">Go somewhere</a>
+                //   </div>
+                // </div>
+                html += `<div class="card">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/How_to_use_icon.svg/1200px-How_to_use_icon.svg.png" class="card-img-top" style="height: 100px; width: 100px" alt="...">poster: ${movie.poster}</img>
+                            <div class="card-body">
+                            <h5 class="card-title">title: ${movie.title}</h5>
+                            <p class="card-text">director: ${movie.director}<br>rating: ${movie.rating}<br>year: ${movie.year}</p>
+                            <button class="remove-btn btn-primary" data-id="${movie.id}">DELETE</button>
+                            </div>
+                        </div>`
             }));
+            // <li></li>
+            // <li>genre: ${movie.genre}</li>
+            // <li>director: ${movie.director}</li>
+            // <li>actors: ${movie.actors}</li>
+            // <li>id: ${movie.id}</li>
+            // <li>plot: ${movie.plot}</li>
+            // <li>poster: ${movie.poster}</li>
+            // <li>rating: ${movie.rating}</li>
+            // <li>year: ${movie.year}</li>
+            // <button className="remove-btn" data-id="${movie.id}">DELETE</button>
+            // <br>
             $("#movie-list").append(html);
         }).then(() => {
         $("#loading-screen").removeClass("d-block").addClass("d-none");
@@ -76,7 +104,7 @@ function displayMovies() {
     })
 }
 
-displayMovies();
+
 //-------------------------------------------------------------------------------------------------
 submit.click(function (e) {
     e.preventDefault();
